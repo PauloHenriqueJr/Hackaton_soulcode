90ac94265d4f8d648d2bb19e37eaf40fe3ea35bd
El Mineiro <84482254+PauloHenriqueJr@users.noreply.github.com>
2021-09-13 10:52:40 -0300

    updates

--- app.js
+++ app.js
@@ -4,8 +4,19 @@ const mongoose = require('mongoose');
 const credentials = require('./credentials.json');
 const router = require('./routers/campaign-router')
 const app = express();
+const porta = 3000
 
-mongoose.connect(credentials.db.mongoDB.host, { useNewUrlParser: true, useUnifiedTopology: true });
+//mongoose.connect(credentials.db.mongoDB.host, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Conexão ao database mongodb
+const database = "mongodb+srv://paulo_henrique:paulo_henrique@cluster0.kypul.mongodb.net/hackaton?retryWrites=true&w=majority" //inserir seu acesso mongodb
+
+mongoose.connect(database, { useUnifiedTopology: true, useNewUrlParser: true  }, (err, client) => {
+  if (err) return console.log(err);
+
+  db = mongoose.connection;
+  console.log("Banco de Dados Conectado");
+});
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname, 'views');
@@ -18,7 +29,12 @@ app.use('/', router)
 
 
 
-//CONEXÃO COM O SERVIDOR
+/*CONEXÃO COM O SERVIDOR
+
 app.listen(credentials.app.port, () => {
     console.log(`Server is running! (port ${credentials.app.port})`);
-})
+})*/
+
+app.listen(porta, () => {
+    console.log("Servidor ativado na porta " + porta);
+  });
